b2953f1c9e419124fc5d388826ba41b25ae33362
rom141 <romain130197@aol.com>
2020-10-21 17:57:04 +0200

    Questions 8

--- TP1_project/Program.cs
+++ TP1_project/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.ExceptionServices;
+using System.Runtime.Serialization;
 
 namespace TP1_project {
     class Program {
@@ -32,6 +34,36 @@ namespace TP1_project {
             int[] test5 = new int[] { -99, 81, 5, 6, -150, 0 };
             Console.WriteLine("{ 0:-99, 1:81, 2:5, 3:6, 4:-150, 5:0 }" + ": [" + GetIndexSmaller(test5) + "]");
             Console.WriteLine("<end: GetIndexSmaller>\n");
+
+            Console.WriteLine("<start: SortBubble>");
+            int[] test6 = new int[] { -99, 81, 5, 6, -150, 0 };
+            int[] bubbleExit = SortBubble(test6);
+            foreach (int value in bubbleExit)
+            {
+                Console.WriteLine(value);
+            }
+            Console.WriteLine("<end: SortBubble>\n");
+
+            Console.WriteLine("<start: SortInsertion>");
+            int[] test7 = new int[] { -99, 81, 5, 6, -150, 0 };
+            int[] insertionExit = SortInsertion(test7);
+            foreach (int value in insertionExit)
+            {
+                Console.WriteLine(value);
+            }
+            Console.WriteLine("<end: SortInsertion>\n");
+
+            Console.WriteLine("<start: Fusion>");
+            List<int> test8_1 = new List<int> { 0, 2, 4, 6 };
+            List<int> test8_2 = new List<int> { 1, 3, 5, 7 };
+            List<int> test8_3 = Fusion(test8_1, test8_2);
+            foreach (int value in test8_3)
+            {
+                Console.WriteLine(value);
+            }
+
+
+            Console.WriteLine("<end: Fusion>\n");
         }
 
         /** <summary>Renvoie une liste de sous-chaîne de characters où chaque sous-chaîne est comprise entre un séparateur.</summary>**/
@@ -86,6 +118,70 @@ namespace TP1_project {
                 }
             }
             return index;
+        }
+
+        static int[] SortBubble(int[] array)
+        {
+            bool modified;
+            do
+            {
+                modified = false;
+                for (int i = 0; i < array.Length -1; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        int temp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = temp;
+                        modified = true;
+                    }
+                }
+            } while (modified);
+
+            return array;
+        }
+
+        static int[] SortInsertion(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    if (array[j - 1] > array[j])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j - 1];
+                        array[j - 1] = temp;
+                    }
+                }
+
+            }
+            return array;
+        }
+
+        static List<int> Fusion(List<int> array1, List<int> array2)
+        {
+            if (array1.Count == 0)
+            {
+                return array2;
+            }
+
+            if (array2.Count == 0)
+            {
+                return array1;
+            }
+            List<int> result = new List<int>();
+            if (array1[0] <= array2[0])
+            {
+                result.Add(array1[0]);
+                result.AddRange(Fusion(array1.GetRange(1,array1.Count -1),array2));
+            }
+            else
+            {
+                result.Add(array2[0]);
+                result.AddRange(Fusion(array2.GetRange(1, array2.Count - 1), array1));
+            }
+            return result;
         }
     }
 }
